5b6b9985bb28707b9f55a670788f220db29ea9c3
Silvanus <selvasilvanus@gmail.com>
2020-08-20 02:50:05 +0530

    Home page video/search section completed

--- js/homePage.js
+++ js/homePage.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    setTimeout(() => {
+        const html = document.querySelector("html")
+        html.style.opacity = 1   
+    }, 700)
+
+    includeHTML();
     const videoElement = document.querySelector("#video")
     videoElement.playbackRate = 0.75
+
 })
