e0f6e5c14bb93f6ef17cfeb44abbff25d151fa10
Silvanus <selvasilvanus@gmail.com>
2020-08-22 00:54:17 +0530

    Completed front end for home page

--- js/homePage.js
+++ js/homePage.js
@@ -3,10 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         const html = document.querySelector("html")
         html.style.opacity = 1   
+        
+
     }, 700)
 
-    includeHTML();
-    const videoElement = document.querySelector("#video")
-    videoElement.playbackRate = 0.75
 
+
+
+        includeHTML();
+        const videoElement = document.querySelector("#video")
+        videoElement.playbackRate = 0.75
+
+        const sr = ScrollReveal({ reset : 1 })
+        sr.reveal(".home-page-box", {
+            delay : 200,
+            origin : "bottom",
+            duration : 600,
+            interval : 300,
+            scale : 0,
+            opacity : 0
+        })
+        sr.reveal(".img", {
+            delay : 200,
+            scale : 0.2,
+            opacity : 0,
+            interval: 200,
+            duration : 600,
+            easing : "ease-out",
+            origin : "bottom"
+        })
 })
