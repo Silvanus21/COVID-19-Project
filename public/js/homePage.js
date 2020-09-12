252c9e7260b52c436078ca1a0721b4afa725826e
Silvanus <selvasilvanus@gmail.com>
2020-09-12 12:50:37 +0530

    Added Loading pages, improvd UI

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded",()=>{setTimeout(()=>{document.querySelector("html").style.opacity=1},700),includeHTML(),document.querySelector("#video").playbackRate=.8;const e=ScrollReveal({reset:!1});e.reveal(".home-box-content",{delay:200,origin:"bottom",duration:800,interval:300,scale:.15,opacity:0}),e.reveal(".img",{delay:150,scale:.2,opacity:0,interval:200,duration:600,easing:"ease-out",origin:"bottom"})});
+document.addEventListener("DOMContentLoaded", () => {
+ 
+    setTimeout(() => {
+        const lw = document.getElementById("loader-wrapper")
+        lw.style.opacity = 0
+        setTimeout(() => {
+          lw.style.display = "none";
+        }, 2000)
+    }, 1500)
+
+    includeHTML()
+
+  (document.querySelector("#video").playbackRate = 0.8);
+  const e = ScrollReveal({ reset: !1 });
+  e.reveal(".home-box-content", {
+    delay: 200,
+    origin: "bottom",
+    duration: 800,
+    interval: 300,
+    scale: 0.15,
+    opacity: 0,
+  }),
+    e.reveal(".img", {
+      delay: 150,
+      scale: 0.2,
+      opacity: 0,
+      interval: 200,
+      duration: 600,
+      easing: "ease-out",
+      origin: "bottom",
+    });
+});
