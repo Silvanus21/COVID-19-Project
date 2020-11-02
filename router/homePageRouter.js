20d167bf7fb5f171e6ab3f3bbac8d867324d89a0
Silvanus <selvasilvanus@gmail.com>
2020-11-02 20:20:43 +0530

    Minor updates

--- router/homePageRouter.js
+++ router/homePageRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
-const url = require("url");
 
 // function to insert commas in long numbers...
 function numberWithCommas(x) {
